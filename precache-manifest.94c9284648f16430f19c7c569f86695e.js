bf0be0e6da0350ca2a37f81a1bbbc98c5a0f834a
Radwan Basqual <radwan.basqual@techsolcom.local>
2020-03-23 22:31:18 -0400

    Updates

---
+++ precache-manifest.94c9284648f16430f19c7c569f86695e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21918f427532981651a9ace49f0102e6",
+    "url": "/reactJS-navetty/index.html"
+  },
+  {
+    "revision": "594c0b2b230cbb8c28e9",
+    "url": "/reactJS-navetty/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "df5d8812a5136b05acb1",
+    "url": "/reactJS-navetty/static/css/main.f52a43bc.chunk.css"
+  },
+  {
+    "revision": "594c0b2b230cbb8c28e9",
+    "url": "/reactJS-navetty/static/js/2.8f989c28.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactJS-navetty/static/js/2.8f989c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5d8812a5136b05acb1",
+    "url": "/reactJS-navetty/static/js/main.ffa195e5.chunk.js"
+  },
+  {
+    "revision": "a32ba26b71a020440c3d",
+    "url": "/reactJS-navetty/static/js/runtime-main.443e6179.js"
+  },
+  {
+    "revision": "89c64320a1d99e95ce301357defe54a5",
+    "url": "/reactJS-navetty/static/media/bg.89c64320.png"
+  },
+  {
+    "revision": "e48830109ca5e4cc1a9ae5da05d01a3e",
+    "url": "/reactJS-navetty/static/media/board.e4883010.woff"
+  },
+  {
+    "revision": "5ccc5763a0b7a3cc913cef24c6183d51",
+    "url": "/reactJS-navetty/static/media/train.5ccc5763.gif"
+  }
+]);
